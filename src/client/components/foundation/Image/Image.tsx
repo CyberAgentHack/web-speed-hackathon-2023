d9de27cdd54653f4c26c2ca0e2c63df77d9ca3b6
ihch <nemu0sou@gmail.com>
2023-06-21 05:38:17 +0900

    画像にloading=lazyを指定する

--- src/client/components/foundation/Image/Image.tsx
+++ src/client/components/foundation/Image/Image.tsx
@@ -13,7 +13,7 @@ export const Image: FC<Props> = ({ fill, ...rest }) => {
       className={classNames(styles.container(), {
         [styles.container__fill()]: fill === true,
       })}
-      loading="eager"
+      loading="lazy"
       {...rest}
     />
   );
