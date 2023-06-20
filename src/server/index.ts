792ab8b79ad79283a3f5f51a4cbd6aad22108c12
ihch <nemu0sou@gmail.com>
2023-06-21 03:48:33 +0900

    レスポンスをgzipで圧縮するようにした
     webpの恩恵を受けるため

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,7 @@ import { koaMiddleware } from '@as-integrations/koa';
 import gracefulShutdown from 'http-graceful-shutdown';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import compress from 'koa-compress';
 import logger from 'koa-logger';
 import route from 'koa-route';
 import send from 'koa-send';
@@ -55,6 +56,8 @@ async function init(): Promise<void> {
       ctx.status = 204;
     }),
   );
+
+  app.use(compress());
 
   app.use(async (ctx, next) => {
       const url = ctx.request.url;
